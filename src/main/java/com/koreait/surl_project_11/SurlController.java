16284af363d7de9eee43413c2d12a99300414104
JerryKiki <gasi758@gmail.com>
2024-11-05 14:40:09 +0900

    S:/, /g/, /all 메서드 추가

--- src/main/java/com/koreait/surl_project_11/SurlController.java
+++ src/main/java/com/koreait/surl_project_11/SurlController.java
@@ -1,10 +1,13 @@
 package com.koreait.surl_project_11;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -24,5 +27,57 @@ public class SurlController {
 
         surls.add(surl);
         return surl;
+    }
+
+    @GetMapping("/s/{body}/**")
+    @ResponseBody
+    public Surl add(
+            @PathVariable String body,
+            HttpServletRequest req
+    ) {
+
+        String url = req.getRequestURI();
+
+        //복잡한 url이 들어왔을 때 queryString이 잘리지 않도록 하기 위함
+        if(req.getQueryString() != null) {
+            url += "?" + req.getQueryString();
+        }
+
+        String[] urlBits = url.split("/", 4);
+
+        System.out.println("Arrays.toString(urlBits) : " + Arrays.toString(urlBits));
+
+        url = urlBits[3];
+
+        Surl surl = Surl.builder()
+                .id(++surlsLastId)
+                .body(body)
+                .url(url)
+                .build();
+
+        surls.add(surl);
+        return surl;
+    }
+
+    @GetMapping("/g/{id}")
+    public String go(
+            @PathVariable long id
+    ){
+        Surl surl = surls.stream()
+                .filter(_surl -> _surl.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        if(surl == null) throw new RuntimeException("%d번 데이터를 찾을 수 없어".formatted(id));
+
+        surl.increaseCount();
+
+        return "redirect:" + surl.getUrl();
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<Surl> getAll() {
+        return surls;
     }
 }
