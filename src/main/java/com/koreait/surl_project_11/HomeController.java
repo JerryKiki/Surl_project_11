2cdf41f667116af87952943538b5a2c410c7bd27
JerryKiki <gasi758@gmail.com>
2024-11-07 10:26:02 +0900

    개발/운영 환경 설정 분리

--- src/main/java/com/koreait/surl_project_11/HomeController.java
+++ src/main/java/com/koreait/surl_project_11/HomeController.java
@@ -1,16 +1,19 @@
 package com.koreait.surl_project_11;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    @Value("${custom.site.name}")
+    private String customSiteName;
 
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Main";
+        return "Main on " + customSiteName;
     }
 
 }
