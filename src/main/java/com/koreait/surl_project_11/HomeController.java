5dc5e7eea05f5af636d6195c8e89e483b16b24d1
JerryKiki <gasi758@gmail.com>
2024-11-07 11:43:55 +0900

    auto test 4

--- src/main/java/com/koreait/surl_project_11/HomeController.java
+++ src/main/java/com/koreait/surl_project_11/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "re-auto test Main on " + customSiteName;
+        return "re-re-auto test Main on " + customSiteName;
     }
 
     @GetMapping("/secretKey")
