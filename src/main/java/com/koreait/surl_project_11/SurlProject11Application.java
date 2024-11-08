79f488e924553204601ecf6ca4228b0d0352197b
JerryKiki <gasi758@gmail.com>
2024-11-08 13:08:48 +0900

    entity에 생성, 수정 날짜 자동으로 기입하도록 어노테이션 추가

--- src/main/java/com/koreait/surl_project_11/SurlProject11Application.java
+++ src/main/java/com/koreait/surl_project_11/SurlProject11Application.java
@@ -2,8 +2,10 @@ package com.koreait.surl_project_11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Article Entity의 @EntityListeners(AuditingEntityListener.class) 어노테이션이 작동하도록 하는 어노테이션
 public class SurlProject11Application {
 
     public static void main(String[] args) {
--- src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
+++ src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
@@ -1,10 +1,12 @@
 package com.koreait.surl_project_11.domain.article.article.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -14,11 +16,16 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Setter
 @NoArgsConstructor //jpa의 어떤 함수들이 이용하도록 하기 위해 필요
 @AllArgsConstructor //builder를 위해 필요
+@EntityListeners(AuditingEntityListener.class) //CreatedDate와 ModifiedDate 어노테이션이 작동하도록 하는 어노테이션
 public class Article {
 
     @Id //이 필드를 PRIMARY KEY로 만들거야
     @GeneratedValue(strategy = IDENTITY) //프라이머리키 값을 자동으로 생성할 때 사용 (==AUTO_INCREMENT)
     private long id;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String body;
