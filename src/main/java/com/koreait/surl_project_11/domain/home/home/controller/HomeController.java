6ec270c93af6a95b93d5b14fdcb367252ff1e540
JerryKiki <gasi758@gmail.com>
2024-11-14 16:47:15 +0900

    포트변경 시도1

--- src/main/java/com/koreait/surl_project_11/domain/home/home/controller/HomeController.java
+++ src/main/java/com/koreait/surl_project_11/domain/home/home/controller/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Main on " + customSiteName + "자동배포 시도 ing >< 포트변경 확인해보자";
+        return "Main on " + customSiteName + "자동배포 시도 ing... 포트변경";
     }
 
     @GetMapping("/secretKey")
