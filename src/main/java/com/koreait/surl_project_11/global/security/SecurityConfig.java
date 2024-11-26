940bf02241bf867906b3dde0c9eed1d6f2290b29
JerryKiki <gasi758@gmail.com>
2024-11-26 15:54:40 +0900

    CustomerAuthenticationFilter 추가

--- src/main/java/com/koreait/surl_project_11/global/security/SecurityConfig.java
+++ src/main/java/com/koreait/surl_project_11/global/security/SecurityConfig.java
@@ -1,14 +1,26 @@
 package com.koreait.surl_project_11.global.security;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
 
+    //로그인을 해도 입구컷이 되는 상황 발생! ==> 내 사이트의 "로그인"이 뭔지 시큐리티는 아직 모른다.
+    //"어떤 상황이 인증된 상황인지"를 알려주기 위한 클래스가 필요함. 유저와 관리자간의 규칙을 security는 모르기 때문에.
+    //우리는 "쿠키에 사용자 정보가 등록되어 있으며, 그 값이 올바른 상태"가 로그인 상태라고 알려줘야함.
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
     //시큐리티(경비같은 느낌으로 이해하면 된다)에게 업무를 알려주는 것.
+    //스프링 시큐리티는 필터로 구현되어 있다.
+    //내가 원하는 필터를 적절한 시점에 실행할 수 있다.
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -33,7 +45,11 @@ public class SecurityConfig {
                         csrf ->
                                 csrf.disable()
                 )
-                .formLogin(formLogin -> formLogin.permitAll()); //로그인 화면은 허용해 (이거 안하면 로그인창 접근도 거부돼서 그냥 엑세스 거부됨만 뜸)
+                .formLogin(formLogin -> formLogin.permitAll()) //로그인 화면은 허용해 (이거 안하면 로그인창 접근도 거부돼서 그냥 엑세스 거부됨만 뜸)
+                //로그인 됐다 안됐다를 판단하기 전에 CustomerAuthenticationFilter 먼저 인식해줘.
+                //CustomerAuthenticationFilter는 쿠키(actorUsername, actorPassword)를 체크해서,
+                //이게 올바르다면 스프링 시큐리티가 이해할 수 있는 방식으로 "이것이 로그인 상태다"라고 알려줘야함.
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
